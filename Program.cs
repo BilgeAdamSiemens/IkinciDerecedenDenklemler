5915fc34ea3c38c247b7e5bb87be4b9872e00999
gulenzek <73951604+gulenzek@users.noreply.github.com>
2020-12-30 21:32:29 +0300

    Yayımlandı.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,41 @@ namespace IkinciDerecedenDenklemler
         static void Main(string[] args)
         {
             Console.WriteLine("İkinci Dereceden Denklem Çözümü:");
+            // ax2 + bx + c = 0
+            double a, b, c;
+            Console.WriteLine("a sayısını giriniz:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("b sayısını giriniz:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("c sayısını giriniz:");
+            c = int.Parse(Console.ReadLine());
+
+            DenklemCozumu(a, b, c);
+        }
+
+        static void DenklemCozumu(double a, double b, double c)
+        {
+            double delta = b * b - 4 * a * c;
+            double x, x1, x2, i;
+
+            if (delta > 0)
+            {
+                x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("Çözüm kümesi: {0,8:f4}, {0,8:f4}", x1, x2);
+            }
+            else if (delta < 0)
+            {
+                delta = -delta;
+                x = -b / (2 * a);
+                i = Math.Sqrt(delta) / (2 * a);
+                Console.WriteLine("Çözüm kümesi: {0,8:f4} + {1,8:f4} i or {2,8:f4} + {3,8:f4} i", x, i, x, i);
+            }
+            else
+            {
+                x = (-b + Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("Çözüm kümesi: {0,8:f4}", x);
+            }
         }
     }
 }
--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,6 @@
 ﻿İkinci Dereceden Denklemler
 
 ax^2 + bx + c = 0 olan denklemlerin çözümü.
+
+Kullanıcı a, b ve c değişkenlerini vererek "ax^2 + bx + c = 0" denklemi oluşturulur.
+Çözüm kümesi ekrana yazılır.
